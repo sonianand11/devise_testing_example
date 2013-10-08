7f31e8af1d119d282a2ff859961a614295a319ae
anand <anand@ubuntu.(none)>
2013-10-08 15:21:21 +0530

    add few more test cases

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -6,4 +6,21 @@ class HomeControllerTest < ActionController::TestCase
     assert_response :redirect
   end
 
+  test "should get home page html after signin" do
+    sign_in @user_one
+    get :index
+    assert_response :success
+    assert_select "h1","Home#index"
+  end
+
+  test "should show signin page after signout" do
+    sign_in @user_one
+    get :index
+    assert_response :success
+    assert_select "h1","Home#index"
+    sign_out @user_one
+    get :index
+    assert_response :redirect
+  end
+
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user count" do
+  	assert_equal 2 , User.count
+  end
+
+  test "user one email is test1@user.com" do
+  	assert_equal "test1@user.com", @user_one.email
+  end
 end
